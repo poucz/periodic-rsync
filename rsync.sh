c092ef238ee22b92ea73bb4d2d6135737e5dda71
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-12-24 15:46:31 +0800

    Force strict host key checking off

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 
 rsync \
   -qrmltyP --delete-delay --delay-updates --delete-excluded \
-  -e "sshpass -p${PASSWORD} ssh -o ServerAliveInterval=60" \
+  -e "sshpass -p${PASSWORD} ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no" \
   "${SOURCE}" \
   /data
   "${TARGET}"
