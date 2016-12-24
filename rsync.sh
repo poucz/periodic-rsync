cfb2d69ba1a2bbc1ee45907b8b750d4a4c4a4c99
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-12-24 16:02:08 +0800

    Sync the directory contents, not the directory itself

--- rsync.sh
+++ rsync.sh
@@ -4,5 +4,5 @@ rsync \
   -qrmltyP --delete-delay --delay-updates --delete-excluded \
   -e "sshpass -p${PASSWORD} ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no" \
   "${SOURCE}" \
-  /data
+  /data/
   "${TARGET}"
