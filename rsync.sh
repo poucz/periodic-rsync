b8ae637696d33df39e8786540e9c97f90ce60f2e
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-12-24 20:17:40 +0800

    Must. Remember. To. Use. Full. Paths.

--- rsync.sh
+++ rsync.sh
@@ -2,5 +2,5 @@
 
 rsync \
   -qrmltyP --delete-delay --delay-updates --delete-excluded \
-  -e "/usr/bin/sshpass -p${PASSWORD} ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no" \
+  -e "/usr/bin/sshpass -p${PASSWORD} /usr/bin/ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no" \
   ${SOURCE} /data/ ${TARGET}
