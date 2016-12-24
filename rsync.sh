295c0e7e3194c910b08c9d61374695153483e256
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-12-24 20:55:59 +0800

    Hard to be verbose when you are told to be quiet

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rsync \
-  -vqrmltyP --delete-delay --delay-updates --delete-excluded \
+  -vrmltyP --delete-delay --delay-updates --delete-excluded \
   -e "/usr/bin/sshpass -p${PASSWORD} /usr/bin/ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no" \
   ${SOURCE} /data/ ${TARGET}
