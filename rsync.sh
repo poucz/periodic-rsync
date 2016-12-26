4aa8e0e8054519d6c6b3dac45cdecdeb22e3e54a
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-12-26 14:41:46 +0800

    Fix test invokation

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check if tempdir has been given
-if [! -z TEMPDIR ]; then
+if [ ! -z TEMPDIR ]; then
   # Create tempdir and update the variable to be a absolute path
   TEMPDIR="$( cd /data ; mkdir -p "$TEMPDIR" ; cd "$TEMPDIR" ; pwd )"
   RSYNC_TEMP="--temp-dir='$(TEMPDIR)'"
